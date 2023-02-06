f00b7a6b2a5582ce57d4fd78d33442a09e608f0d
clariiemesq <clari.mesquitalves@gmail.com>
2023-02-06 15:46:19 -0300

    textos

---
+++ aboutText.py
@@ -0,0 +1,10 @@
+about_text = "Esse aplicativo foi desenvolvido com o objetivo " \
+             "de facilitar a organização de lembretes tornando mais " \
+             "acessível para pessoas cegas e com baixa visão. Para isso, " \
+             "esse aplicativo pode ser controlado por voz ou por meio de " \
+             "botões coloridos e chamativos que possuem sua identificação " \
+             "com fontes grandes para facilitar a leitura. Lembrando que, para" \
+             " o programa captar melhor a sua voz e para uma melhor experiência," \
+             " é recomendado o uso de fones de ouvido. Atenciosamente, Desenvolvedores. " \
+             "Desenvolvedores: Albert Danilo, Clarice Mesquita, Grasielly Rayara, Jackson Webber, " \
+             "José Roberto e Lara Rayssa."
---
+++ helpText_normal.py
@@ -0,0 +1,28 @@
+add_reminderButton = "Adicionar Lembrete. Para adicionar um lembrete, digite " \
+                     "no espaço destinado para o conteúdo do lembrete. Depois, " \
+                     "determine o número de dias para o lembrete ficar " \
+                     "armazenado no aplicativo. Por fim, clique em confirmar." \
+                     "Caso queira cancelar, clique em cancelar."
+
+list_reminderButton = "Listar Lembretes. Assim que você clicar no botão " \
+                      "Listar lembretes... você terá acesso à lista de lembretes " \
+                      "organizada por data de expiração (primeiro os que estão" \
+                      " mais perto de expirar e por último os que tem o período" \
+                      " de duração maior). Para ouvir o conteúdo, " \
+                      "basta clicar em cima do lembrete que você quer. " \
+                      "Para navegar entre as páginas e ver mais lembretes cadastrados" \
+                      "utilize os botões próxima página e voltar. Para sair da lista de lembretes," \
+                      "basta clicar em OK."
+
+delete_reminderButton = "Excluir Lembrete. Assim que você clicar no botão " \
+                      "Listar lembretes... você terá acesso à lista de lembretes " \
+                      "organizada por data de expiração (primeiro os que estão" \
+                      " mais perto de expirar e por último os que tem o período" \
+                      " de duração maior). Para excluir um lembrete basta clicar no" \
+                      " lembrete desejado e clicar em iés. Caso não queira excluir, " \
+                      "clique em nou. Para navegar entre as páginas e ver" \
+                      " mais lembretes cadastrados utilize os botões próxima página e" \
+                      " voltar. Caso não tenha mais nenhum lembrete para excluir" \
+                      ", clique em Ok para sair."
+
+exitButton = "Sair. Para sair, basta clicar no botão sair."
