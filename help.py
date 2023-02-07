37ae4a854e30102eff00a9675ae38e0d19a62988
Robertoskb <97955337+Robertoskb@users.noreply.github.com>
2023-02-06 21:40:35 -0300

    botões conectados

--- help.py
+++ help.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import QtWidgets
 from Interfaces.helpWindow import Ui_helpWindow
+from say_it import say
+import helpText
 
 
 class HelpWindow(QMainWindow, Ui_helpWindow):
@@ -8,6 +10,13 @@ class HelpWindow(QMainWindow, Ui_helpWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        self.helpSobre_Button.clicked.connect(lambda: say(helpText.aboutButton))
+        self.helpAjuda_Button.clicked.connect(lambda: say(helpText.helpButton))
+        self.helpComandos_Button.clicked.connect(lambda: say(helpText.commandButton))
+        self.helpListar_Button.clicked.connect(lambda: say(helpText.list_reminderButton))
+        self.helpExcluir_Button.clicked.connect(lambda: say(helpText.delete_reminderButton))
+        self.helpAdicionar_Button.clicked.connect(lambda: say(helpText.add_reminderButton))
 
 
 if __name__ == "__main__":
